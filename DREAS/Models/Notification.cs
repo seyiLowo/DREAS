659bb46b01b86d87422244db79002b7a969fcc4b
christian Ejike <ejikec2@gmail.com>
2019-02-21 09:17:52 +0100

    created notification class

---
+++ DREAS/Models/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DREAS.Models
+{
+    public class Notification: BaseModel
+    {
+        public Contact Email { get; set; }
+        public int PropertyID { get; set; }
+        public Enquiry Message { get; set; }
+        public Guid UserID { get; set; }
+
+    }
+}
