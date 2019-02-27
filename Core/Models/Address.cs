7c0ac9552a53eebf828ce77e0455440778299c5b
christian Ejike <ejikec2@gmail.com>
2019-02-27 16:35:40 +0100

    Added some input validations

--- Core/Models/Address.cs
+++ Core/Models/Address.cs
@@ -1,11 +1,18 @@
-﻿namespace DREAS.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DREAS.Models
 {
     public class Address
     {
+        [Required(ErrorMessage = "HouseNO is required")]
         public string HouseNo { get; set; }
+        [Required(ErrorMessage = "StreetName is required")]
         public string StreetName { get; set; }
+        [Required(ErrorMessage = "City is required")]
         public string City { get; set; }
+        [Required(ErrorMessage = "LGA is required")]
         public string LGA { get; set; }
+        [Required(ErrorMessage = "State is required")]
         public string State { get; set; }
 
     }
--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -10,10 +10,12 @@ namespace DREAS.Models
         [StringLength(128,ErrorMessage ="Username has exceeded the maximumn limit")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Password is required")]
-
+        [StringLength(8, ErrorMessage = "Password must not be less than 8 characters")]
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
         public Boolean IsBlocked { get; set; } = false;
         public DateTime BlockedDate { get; set; }
